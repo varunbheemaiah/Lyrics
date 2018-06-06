961163d58c85f6dfdd0c6e279060f73b36e8923a
varunbheemaiah <varunbheemaiah9@gmail.com>
2018-06-06 23:00:14 +0530

    LinkList

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def index():
     return render_template('index.html', songs=songs, links=links)
 
 if __name__=='__main__':
+    app.jinja_env.globals.update(zip=zip)
     app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
